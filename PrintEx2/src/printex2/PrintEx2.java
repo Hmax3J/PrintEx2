4fe3c625235edac6b044cd6a357611756b4e68f1
Hmax3J <Hmax3J@gamil.com>
2020-10-23 03:17:42 +0900

    2

---
+++ PrintEx2/src/printex2/PrintEx2.java
@@ -0,0 +1,14 @@
+package printex2;
+
+public class PrintEx2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");    
+ 
+	}
+
+}
